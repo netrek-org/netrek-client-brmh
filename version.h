aa11f9d1101556ae274d57e91207fde0f3bcf253
ahn <ahn>
2000-03-23 03:44:26 +0000

    Version update to 2.4.

--- version.h
+++ version.h
@@ -3,11 +3,11 @@
 /* program version number stuff */
 
 #ifdef nodef
-static char    *Program_Version = "$Revision: 1.3 $   Compiled on $Date: 2000/03/23 03:20:25 $";
+static char    *Program_Version = "$Revision: 1.4 $   Compiled on $Date: 2000/03/23 03:44:26 $";
 #endif
 
 #define PATCHLEVEL     0
-#define VERSION	       "2.3.1"
+#define VERSION	       "2.4.0"
 #define MAINTENANCE    "vanilla-list@us.netrek.org"
 #define URL_NETREK     "http://www.netrek.org/"
 #define URL_XTREKRC    "ftp://ftp.netrek.org/pub/netrek/clients/brmh/bin/BRMH-xtrekrc"
